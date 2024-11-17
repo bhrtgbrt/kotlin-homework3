d325ee44e84a41da54b167edc076a065d9d28bb6
bhrtgbrt <chengandrew0513@gmail.com>
2024-11-17 12:36:23 +0800

    1117

---
+++ Lab7/app/src/main/java/com/example/lab7/Item.kt
@@ -0,0 +1,7 @@
+package com.example.lab7
+
+data class Item(
+    val photo: Int,
+    val name: String,
+    val price: Int
+)
---
+++ Lab8/app/src/main/java/com/example/lab8/Contact.kt
@@ -0,0 +1,6 @@
+package com.example.lab8
+
+data class Contact (
+    val name: String,
+    val phone: String
+)
